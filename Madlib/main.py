631b354ee2d763d9f55e9745f0603fb827c023c9
AuntieEm <Emily.davis.5828@gmail.com>
2016-07-02 08:29:06 -0500

    More updates
    loop
    conditional statements
    function

--- Madlib/main.py
+++ Madlib/main.py
@@ -1,3 +1,10 @@
+#start 3:23am july 2
+#pause 5:11am july 2
+#restart 7:15am july 2
+#pause
+#restart
+#end
+
 print "Welcome to Mad Libs with Emily!"
 print "Please follow the instructions below."
 
@@ -5,20 +12,57 @@ print "Please follow the instructions below."
 holiday = ["Kawanza", "Groundhog Day"]
 
 #collect numbers from user
-number_one = raw_input("Enter a number between 1-20:  ")
-number_two = raw_input("Enter your favorite number:  ")
-number_three = raw_input("Enter your least favorite number:  ")
+number_one = float(input("Enter a number between 1-20:  "))
+#conditional statements
+if number_one > 20:
+    print("You must choose a number lower than 20.")
+    number_one = float(input("Enter a number between 1-20:  "))
+else:
+    print("Good choice.")
+
+number_two = float(input("Enter your favorite number:  "))
+number_three = float(input("Enter a number between 2-15 and greater than 2:  "))
+
+#conditional statements
+if number_three > 15:
+    print("You must choose a number less than 15 and greater than 2.")
+    number_three = float(input("Enter your new number here:  "))
+elif number_three < 2:
+    print("You must choose a number greater than 2 and less than 15.")
+    number_three = float(input("Enter your new number here:  "))
+else:
+    print("Good choice.")
 
 #dictionary
 animals = dict()
 animals = {"one":"pheasant", "two":"fox", "three":"antelope"}
 
 #mathmatical operators
-number_four = 57 / 7
-number_five = 33 / 11
+number_four = number_one + number_two
+number_five = number_three * number_one
+
+person_one = raw_input("Enter a person's name:  ")
+person_two = raw_input("Enter another person's name:  ")
+person_three = raw_input("Enter another person's name:  ")
+
+#function that returns a value and has at least 2 paramaters
+def happyHoliday(person):
+    print("Happy Thanks Giving " + person + "!")
+def run():
+    happyHoliday(person_one)
+    happyHoliday(person_two)
+    happyHoliday(person_three)
+run()
+
+
+#one loop
+family = ["Grandma", "Grandpa", "Aunt", "Uncle"]
+for f in family:
+    print "I love my " + f
+
+
 
 #collect info from user
-person_one = raw_input("Enter a person's name:  ")
 verb_past_one = raw_input("Enter a past tense verb:  ")
 adjective_one = raw_input("Enter an adjective:  ")
 transport = raw_input("Choose a mode of transportation:  ")
@@ -27,13 +71,12 @@ plural_noun_two = raw_input("Enter a plural noun that's nature related:  ")
 family_one = raw_input("Enter a family relation:  ")
 adjective_two = raw_input("Enter another adjective:  ")
 greeting_one = raw_input("Enter a kind of greeting:  ")
-person_two = raw_input("Enter another person's name:  ")
 adjective_three = raw_input("Enter another adjective:  ")
 object_one = raw_input("Enter an object that can be found in your home:  ")
 plural_noun_three = raw_input("Enter anothe plural noun:  ")
 plural_noun_four = raw_input("Enter another plural noun:  ")
 family_two = raw_input("Enter another type of family member:  ")
-sport_one = raw_input("What's your favorite sport:  ")
+sport_one = raw_input("Enter your favorite sport:  ")
 exclamation_one = raw_input("What do you shout when you're upset at an event:  ")
 noun_one = raw_input("Enter a noun:  ")
 food_one = raw_input("What is your favorite food:  ")
@@ -41,7 +84,6 @@ noun_two = raw_input("Enter another noun:  ")
 noun_three = raw_input("Enter another noun:  ")
 adjective_four = raw_input("Enter another adjective:  ")
 family_three = raw_input("Enter another family relation:  ")
-person_three = raw_input("Enter another person's name:  ")
 trick = raw_input("Whats a crazy trick you can do:  ")
 
 
@@ -75,3 +117,5 @@ did that crazy {trick}.
 
 madlib = madlib.format(**locals())
 print madlib
+
+
