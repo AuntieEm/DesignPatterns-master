cbb943881b46a723e916abf1fdb65017e7506205
AuntieEm <Emily.davis.5828@gmail.com>
2016-07-02 04:50:43 -0500

    Madly Updates
    number inputs
    strings
    holiday array
    animals dictionary

--- Madlib/main.py
+++ Madlib/main.py
@@ -1,42 +1,64 @@
-#collect info from user
+print "Welcome to Mad Libs with Emily!"
+print "Please follow the instructions below."
 
+#array
+holiday = ["Kawanza", "Groundhog Day"]
+
+#collect numbers from user
+number_one = raw_input("Enter a number between 1-20:  ")
+number_two = raw_input("Enter your favorite number:  ")
+number_three = raw_input("Enter your least favorite number:  ")
+
+animals = dict()
+animals = {"one":"pheasant", "two":"fox", "three":"antelope"}
+
+#collect info from user
+person_one = raw_input("Enter a person's name:  ")
+verb_past_one = raw_input("Enter a past tense verb:  ")
 adjective_one = raw_input("Enter an adjective:  ")
-adjective_two = raw_input("Enter an adjective:  ")
-room_one = raw_input("Enter a type of room:  ")
-pluralnoun_one = raw_input("Enter a plural noun:  ")
-pluralnoun_two = raw_input("Enter a plural noun:  ")
+transport = raw_input("Choose a mode of transportation:  ")
+plural_noun_one = raw_input("Enter a plural noun:  ")
+plural_noun_two = raw_input("Enter a plural noun that's nature related:  ")
+family_one = raw_input("Enter a family relation:  ")
+adjective_two = raw_input("Enter another adjective:  ")
+greeting_one = raw_input("Enter a kind of greeting:  ")
+person_two = raw_input("Enter another person's name:  ")
+adjective_three = raw_input("Enter another adjective:  ")
+object_one = raw_input("Enter an object that can be found in your home:  ")
+plural_noun_three = raw_input("Enter anothe plural noun:  ")
+plural_noun_four = raw_input("Enter another plural noun:  ")
+family_two = raw_input("Enter another type of family member:  ")
+sport_one = raw_input("What's your favorite sport:  ")
+exclamation_one = raw_input("What do you shout when you're upset at an event:  ")
 noun_one = raw_input("Enter a noun:  ")
-noun_two = raw_input("Enter a noun:  ")
-noun_three = raw_input("Enter a noun:  ")
-adjective_three = raw_input("Enter an adjective:  ")
-noun_four = raw_input("Enter a noun:  ")
-number_one = raw_input("Enter a number:  ")
-pluralnoun_three = raw_input("Enter a plural noun")
-noun_five = raw_input("Enter a noun:  ")
-color_one = raw_input("Enter a color:  ")
-color_two = raw_input("Enter a color:  ")
-living_one = raw_input("Enter something alive:  ")
-person_one = raw_input("Enter person in room:  ")
-person_two = raw_input("Enter person in room:  ")
+food_one = raw_input("What is your favorite food:  ")
+noun_two = raw_input("Enter another noun:  ")
+noun_three = raw_input("Enter another noun:  ")
+adjective_four = raw_input("Enter another adjective:  ")
+
 
 
 madlib = '''
-SELECTING A TREE
+THANKSGIVING MAD LIBS
 
-No Christmas season can be really {adjective_one} unless you have
-a/an {adjective_two} tree in your {room_one}. If you live in
- a city, you will see many vacant {pluralnoun_one} filled with hundreds
- of {pluralnoun_two} for sale. If you live in the country, you can get
- your own {noun_one} right out of the forest. Go out with a/an
- {noun_two} and {noun_three}, and when you see a/an {adjective_three}
- tree you like, you can dig it up and plant it in a/an {noun_four}.
- Then you can use it for {number_one} years. To make sure your tree
- is fresh, shake the branches and see if the {pluralnoun_three} fall off onto
- the {noun_five}. And make sure the tree is very {color_one}.
- Nothing looks worse than a/an {color_two} tree. Just follow these
- directions and you can have a perfectly beautiful {living_one} in
- your front room for weeks. Remember, poems and Mad Libs are made
- by fools like {person_one}, but only {person_two} can make a tree.
+Today we are celebrating ''' + holiday [0] + ''' dinner with the whole family
+at {person_one}'s House. Earlier today we all {verb_past_one}
+into the {adjective_one} {transport} and drove over the {number_one}
+{plural_noun_one} and through the {plural_noun_two} to get here. Once we arrived,
+my {family_one} greeted us with a big {adjective_two}
+{greeting_one}. {person_two} is also here and really made sure
+things were looking {adjective_three}. They had decorated the {object_one}
+with {number_two} {plural_noun_three}. It was all so very festive! Now we are just
+waiting for the ''' + animals["one"] + ''' to come out of the oven. I always eat my
+''' + animals["two"] + ''' with a side of mashed {plural_noun_four}.
+My {family_two} is watching {sport_one} on TV. They
+always shout {exclamation_one} when their team scores a {noun_one}.
+Horray! Only {number_three} more minutes until the {food_one} will be
+ready to eat. Don't forget to save room for dessert. My grandma makes the best
+{noun_two} pies. They smell like {noun_three}. I'm so full now I
+feel like an {adjective_four} ''' + animals["three"] + '''! Happy ''' + holiday [1] + '''!
+
+
 '''
 
 
